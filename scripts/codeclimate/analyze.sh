c460d0b79f2b4f31730c977cda6d1874fc04b5f7
Saad Shakil <sshakil@pivotal.io>
2016-05-06 16:17:58 -0400

    fix docker run command
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd code
-docker run \
-  -v /var/run/docker.sock:/var/run/docker.sock
-  -v $(which docker):$(which docker)
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
--- scripts/codeclimate/install.sh
+++ scripts/codeclimate/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker run \
-  -v /var/run/docker.sock:/var/run/docker.sock
-  -v $(which docker):$(which docker)
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate help
