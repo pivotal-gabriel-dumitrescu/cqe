6a78d2e344ebc82295f6a00553932a89a15ceac0
Saad Shakil <sshakil@pivotal.io>
2016-05-06 14:29:12 -0400

    update codeclimate steps
    Signed-off-by: Saad Shakil <sshakil@pivotal.io>

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-set -ex
-
-# engine=$1
-
 cd code
-
-# docker run \
-#   --interactive --tty --rm \
-#   --env CODECLIMATE_CODE="$PWD" \
-#   --volume "$PWD":/code \
-#   --volume /var/run/docker.sock:/var/run/docker.sock \
-#   --volume /tmp/cc:/tmp/cc \
-#   codeclimate/codeclimate analyze -f text -e "$engine" > report-"$engine".txt
 
 docker run \
   --interactive --tty --rm \
