82af4c244c4d0058b2e3672547e427e0c32d81e2
Saad Shakil <sshakil@pivotal.io>
2016-05-06 17:05:28 -0400

    update install
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- scripts/codeclimate/install.sh
+++ scripts/codeclimate/install.sh
@@ -3,9 +3,7 @@
 cd codeclimate-cli
 
 # To install, run the following command as root:
-curl -sSL -O https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz && sudo tar zxf docker-1.11.1.tgz -C /
-# Then start docker in daemon mode:
-sudo /usr/local/bin/docker daemon
+curl -sSL -O https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz && sudo tar zxf docker-1.11.1.tgz -C && sudo /usr/local/bin/docker daemon
 
 docker -d
 sudo docker -d
