7ff44d76098c7255b503882a35723a2d8dc76957
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-06 14:43:45 -0400

    update docker command in analyze.sh
    Signed-off-by: Saad Shakil <sshakil@pivotal.io>

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -2,4 +2,9 @@
 
 cd code
 
-docker run codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
+docker run \
+  --tty --rm \
+  --volume .:/code \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume /tmp/cc:/tmp/cc \
+  codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
