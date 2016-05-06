adf58d7c4e8ed9d07966286192a8c672d3115761
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-06 14:59:27 -0400

    update analyze.sh
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -3,8 +3,4 @@
 cd code
 
 sudo docker -d
-sudo docker run \
-  --tty --rm \
-  --volume .:/code \
-  --volume /tmp/cc:/tmp/cc \
-  codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
+docker run codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
