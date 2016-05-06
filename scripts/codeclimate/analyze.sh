8d16a0ba52baa5305474ddcd815f9d4df4d2d9bf
Saad Shakil <sshakil@pivotal.io>
2016-05-06 17:55:40 -0400

    update codeclimate steps
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 cd code
-docker run -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $(which docker):$(which docker) \
-  -ti codeclimate/codeclimate analyze -f text -e fixme > report-fixme.txt
+
+codeclimate analyze -f text -e duplication > report-duplication.txt
+codeclimate analyze -f text -e eslint > report-eslint.txt
+codeclimate analyze -f text -e fixme > report-fixme.txt
+codeclimate analyze -f text -e nodesecurity > report-nodesecurity.txt
+codeclimate analyze -f text -e requiresafe > report-requiresafe.txt
--- scripts/codeclimate/install.sh
+++ scripts/codeclimate/install.sh
@@ -2,8 +2,6 @@
 
 cd codeclimate-cli
 
-docker pull codeclimate/codeclimate
-
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
-  -ti codeclimate/codeclimate help
+  -ti codeclimate/codeclimate version
