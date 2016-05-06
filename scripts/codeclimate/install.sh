83dda4fb5a9134b0a82d6b9b4cbf3d3b0c862aa6
Saad Shakil <sshakil@pivotal.io>
2016-05-06 16:43:16 -0400

    update install.sh
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- scripts/codeclimate/install.sh
+++ scripts/codeclimate/install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+cd codeclimate-cli
+
+docker images
+docker ps
+
 docker pull codeclimate/codeclimate
+
+docker images
+docker ps
+
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate help
