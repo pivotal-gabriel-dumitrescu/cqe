7717153e1c4931c0a4e049bad28f78a50df28075
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-06 16:20:16 -0400

    fix docker run command
    Signed-off-by: Saad Shakil <sshakil@pivotal.io>

--- scripts/codeclimate/install.sh
+++ scripts/codeclimate/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+docker pull codeclimate/codeclimate
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate help
