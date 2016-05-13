405603e2753131bc30b49176b81b6adadc089e4b
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-13 14:23:51 -0400

    [WIP] Fix docker sock issue

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -6,6 +6,14 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate version
 
+docker run \
+  --interactive --tty --rm \
+  --env CODECLIMATE_CODE="$PWD" \
+  --volume "$PWD":/code \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume /tmp/cc:/tmp/cc \
+  codeclimate/codeclimate help
+
 #codeclimate analyze -f text -e duplication > report-duplication.txt
 #codeclimate analyze -f text -e eslint > report-eslint.txt
 #codeclimate analyze -f text -e fixme > report-fixme.txt
