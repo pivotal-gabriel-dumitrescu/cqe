f3275ca63e2ef958632aa81160c02bfdbe5cc18d
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-13 14:11:30 -0400

    [WIP] Fix docker sock issue

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -2,7 +2,7 @@
 
 cd codeclimate-cli
 
-sudo docker run -v /var/run/docker.sock:/var/run/docker.sock \
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate version
 
