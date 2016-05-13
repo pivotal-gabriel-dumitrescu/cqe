bb8c87887cd4cf09d40d68a626784a3688935c68
Gabriel Dumitrescu <gdumitrescu@pivotal.io>
2016-05-13 14:03:04 -0400

    [WIP] Fix docker sock issue

--- scripts/codeclimate/analyze.sh
+++ scripts/codeclimate/analyze.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-cd code
+cd codeclimate-cli
 
-sudo docker start
-
-docker run -v /var/run/docker.sock:/var/run/docker.sock \
+sudo docker run -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):$(which docker) \
   -ti codeclimate/codeclimate version
 
-codeclimate analyze -f text -e duplication > report-duplication.txt
-codeclimate analyze -f text -e eslint > report-eslint.txt
-codeclimate analyze -f text -e fixme > report-fixme.txt
-codeclimate analyze -f text -e nodesecurity > report-nodesecurity.txt
-codeclimate analyze -f text -e requiresafe > report-requiresafe.txt
+#codeclimate analyze -f text -e duplication > report-duplication.txt
+#codeclimate analyze -f text -e eslint > report-eslint.txt
+#codeclimate analyze -f text -e fixme > report-fixme.txt
+#codeclimate analyze -f text -e nodesecurity > report-nodesecurity.txt
+#codeclimate analyze -f text -e requiresafe > report-requiresafe.txt
